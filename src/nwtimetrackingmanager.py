622405fd65bce5561eca96810acd9d2db13681f9
Rubèn <numbworks@gmail.com>
2023-08-21 19:43:38 +0200

    #4 SettingCollection: added.

--- src/nwtimetrackingmanager.py
+++ src/nwtimetrackingmanager.py
@@ -28,7 +28,55 @@ class YearlyTarget():
         
         self.year = year
         self.hours = hours
- 
+class SettingCollection():
+
+    '''Represents a collection of settings.'''
+
+    read_years : list[int]
+    yearly_targets : list[YearlyTarget]
+    excel_path : str
+    excel_books_skiprows : int
+    excel_books_nrows : int
+    excel_books_tabname : str
+    excel_null_value : str
+    n_generic : int
+    n_by_month : int
+    show_sessions_df : bool
+    show_tts_by_month_upd_df : bool
+    show_cumulative_df : bool
+    now : datetime
+
+    def __init__(
+        self,
+        read_years : list[int],
+        yearly_targets : list[YearlyTarget],
+        excel_path : str,
+        excel_books_skiprows : int,
+        excel_books_nrows : int,
+        excel_books_tabname : str,
+        excel_null_value : str,
+        n_generic : int,
+        n_by_month : int,
+        show_sessions_df : bool,
+        show_tts_by_month_upd_df : bool,
+        show_cumulative_df : bool,
+        now : datetime
+        ):
+
+        self.read_years = read_years
+        self.yearly_targets = yearly_targets
+        self.excel_path = excel_path
+        self.excel_books_skiprows = excel_books_skiprows
+        self.excel_books_nrows = excel_books_nrows
+        self.excel_books_tabname = excel_books_tabname
+        self.excel_null_value = excel_null_value
+        self.n_generic = n_generic
+        self.n_by_month = n_by_month
+        self.show_sessions_df = show_sessions_df
+        self.show_tts_by_month_upd_df = show_tts_by_month_upd_df
+        self.show_cumulative_df = show_cumulative_df
+        self.now = now 
+
 # FUNCTIONS
 
 # MAIN
