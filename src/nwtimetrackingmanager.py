d0f8a06991c38a8ac024533a774421cee96e53a4
Rubèn <numbworks@gmail.com>
2023-09-30 00:11:47 +0200

    #8 * => get_raw_tt_by_year_month_sp().

--- src/nwtimetrackingmanager.py
+++ src/nwtimetrackingmanager.py
@@ -344,36 +344,13 @@ def calculate_percentage(part : float, whole : float, rounding_digits : int = 2)
     prct = round(number = prct, ndigits = rounding_digits)
 
     return prct
-def get_tt_by_year_month_sp(
-        sessions_df : DataFrame, 
-        years : list[int], 
-        yearly_targets : list[YearlyTarget], 
-        software_project_names : list[str]) -> DataFrame:
+def get_raw_tt_by_year_month_sp(sessions_df : DataFrame, years : list[int], software_project_names : list[str]) -> DataFrame:
     
     '''
-        [tt_df] [0]
-
-                Year	Month	ProjectName	        ProjectVersion	Duration
-            0	2023	4	    nwtraderaanalytics	2.0.0	        0 days 09:15:00
-            1	2023	5	    NW.AutoProffLibrary	1.0.0	        0 days 09:30:00
-            ...
-
-        [dme_df]
-
-                Year	Month	MonthlyDuration
-            0	2023	4	    0 days 09:15:00
-            1	2023	5	    0 days 09:30:00
-            ...
-
-        [tt_df] [1]
-
-                Year	Month	ProjectName	            ProjectVersion	Duration	    MonthlyDuration
-            ...
-            2	2023	6	    NW.SimCorpExercise	    1.0.0	        0 days 05:00:00	0 days 11:45:00
-            3	2023	6	    nwreadinglistmanager	1.0.0	        0 days 06:45:00	0 days 11:45:00
-            ...
-    
-             # DevelopmentMonthlyEffort
+            Year	Month	ProjectName	        ProjectVersion	Effort
+        0	2023	4	    nwtraderaanalytics	2.0.0	        0 days 09:15:00
+        1	2023	5	    NW.AutoProffLibrary	1.0.0	        0 days 09:30:00
+        ...
     '''
 
     tt_df : DataFrame = sessions_df.copy(deep = True)
@@ -384,36 +361,23 @@ def get_tt_by_year_month_sp(
     condition_two : Series = (sessions_df[cn_is_software_project] == True)
     tt_df = tt_df.loc[condition_one & condition_two]
 
-    cn_month : str = "Month"
-    cn_duration : str = "Duration"
-    cn_effort : str = "Effort"
-    cn_dme : str = "DME"
-    dme_df : DataFrame = tt_df.copy(deep = True)
-    dme_df[cn_effort] = dme_df[cn_duration].apply(lambda x : convert_string_to_timedelta(td_str = x))
-    dme_df = dme_df.groupby(by = [cn_year, cn_month])[cn_effort].sum().sort_values(ascending = [False]).reset_index(name = cn_effort)
-    dme_df = dme_df.sort_values(by = [cn_year, cn_month]).reset_index(drop = True)
-    dme_df.rename(columns = {cn_effort : cn_dme}, inplace = True)
-
     cn_descriptor : str = "Descriptor"
     cn_project_name : str = "ProjectName"
     cn_project_version : str = "ProjectVersion"
     tt_df[cn_project_name] = tt_df[cn_descriptor].apply(lambda x : extract_software_project_name(descriptor = x))
     tt_df[cn_project_version] = tt_df[cn_descriptor].apply(lambda x : extract_software_project_version(descriptor = x))
 
-    tt_df[cn_effort] = tt_df[cn_duration].apply(lambda x : convert_string_to_timedelta(td_str = x))
-    tt_df = tt_df.groupby(by = [cn_year, cn_month, cn_project_name, cn_project_version])[cn_effort].sum().sort_values(ascending = [False]).reset_index(name = cn_effort)
+    cn_month : str = "Month"
+    cn_duration : str = "Duration"
+    tt_df[cn_duration] = tt_df[cn_duration].apply(lambda x : convert_string_to_timedelta(td_str = x))
+    tt_df = tt_df.groupby(by = [cn_year, cn_month, cn_project_name, cn_project_version])[cn_duration].sum().sort_values(ascending = [False]).reset_index(name = cn_duration)
     tt_df = tt_df.sort_values(by = [cn_year, cn_month, cn_project_name, cn_project_version]).reset_index(drop = True)
   
-    tt_df = pd.merge(
-        left = tt_df, 
-        right = dme_df, 
-        how = "inner", 
-        left_on = [cn_year, cn_month], 
-        right_on = [cn_year, cn_month]
-        )
-    
-    cn_effort_prct : str = "%_DME"
-    tt_df[cn_effort_prct] = tt_df.apply(lambda x : calculate_percentage(part = x[cn_effort], whole = x[cn_dme]), axis = 1)
+    cn_effort : str = "Effort"
+    tt_df.rename(columns = {cn_duration : cn_effort}, inplace = True)
+
+    condition_three : Series = (tt_df[cn_project_name].isin(values = software_project_names))
+    tt_df = tt_df.loc[condition_three]
 
     return tt_df
 
