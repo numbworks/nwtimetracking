2522fe8ab7986b644d876cce3fe133665f53dc8a
Rubèn <numbworks@gmail.com>
2023-12-01 14:37:32 +0100

    #18 GetSessionsDatasetTestCase - Iteration 1: added.

--- tests/nwtimetrackingmanagertests.py
+++ tests/nwtimetrackingmanagertests.py
@@ -1,12 +1,14 @@
 # GLOBAL MODULES
 import unittest
-from unittest import mock
+import pandas as pd
 from unittest.mock import patch
+from pandas import DataFrame
 
 # LOCAL MODULES
 import sys, os
 sys.path.append(os.path.dirname(__file__).replace('tests', 'src'))
 import nwtimetrackingmanager as nwttm
+from nwtimetrackingmanager import SettingCollection
 
 # TEST CLASSES
 class GetDefaultTimeTrackingPathTestCase(unittest.TestCase):
@@ -24,6 +26,33 @@ class GetDefaultTimeTrackingPathTestCase(unittest.TestCase):
 
         # Assert
         self.assertEqual(expected, actual)
+class GetSessionsDatasetTestCase(unittest.TestCase):
+
+    def test_getsessionsdataset_shouldreturnexpecteddataframe_wheninvoked(self):
+
+        # Arrange
+        excel_data_dict : dict = {
+            "Date": "2015-10-31",
+            "StartTime": "",
+            "EndTime": "",
+            "Effort": "8h 00m",
+            "Hashtag": "#untagged",
+            "Descriptor": "",
+            "IsSoftwareProject": "False",
+            "IsReleaseDay": "False",
+            "Year": "2015",
+            "Month": "10"
+            }
+        excel_data_df : DataFrame = pd.DataFrame(data = excel_data_dict, index=[0])
+        setting_collection : SettingCollection = SettingCollection()
+
+        # Act
+        with patch.object(pd, 'read_excel', return_value = excel_data_df) as mocked_context:
+            actual : str = nwttm.get_sessions_dataset(setting_collection = setting_collection)
+
+        # Assert
+        pass
+
 
 # MAIN
 if __name__ == "__main__":
