ffc778721f560b6d9f83af9dab9adeba1493eff8
Rubèn <numbworks@gmail.com>
2024-12-17 16:02:56 +0000

    #42 setup.py: setup() moved under main.

--- src/setup.py
+++ src/setup.py
@@ -9,22 +9,23 @@ MODULE_NAME : str = "nwtimetracking"
 MODULE_VERSION : str = "4.5.0"
 
 # SETUP
-setup(
-    name = MODULE_NAME,
-    version = MODULE_VERSION,
-    description = "An application designed to run automated data analysis tasks on 'Time Tracking.xlsx'.",
-    author = "numbworks",
-    url = f"https://github.com/numbworks/{MODULE_NAME}",
-    py_modules = [ MODULE_NAME ],
-    install_requires = [
-        "numpy>=2.1.2",
-        "pyarrow>=17.0.0",
-        "pyarrow>=17.0.0",
-        "pandas>=2.2.3",
-        "requests>=2.32.3",
-        "tabulate>=0.9.0",
-        "nwshared @ git+https://github.com/numbworks/nwshared.git@v1.8.0#egg=nwshared&subdirectory=src"
-    ],
-    python_requires = ">=3.12",
-    license = "MIT"
-)
+if __name__ == "__main__":
+    setup(
+        name = MODULE_NAME,
+        version = MODULE_VERSION,
+        description = "An application designed to run automated data analysis tasks on 'Time Tracking.xlsx'.",
+        author = "numbworks",
+        url = f"https://github.com/numbworks/{MODULE_NAME}",
+        py_modules = [ MODULE_NAME ],
+        install_requires = [
+            "numpy>=2.1.2",
+            "pyarrow>=17.0.0",
+            "pyarrow>=17.0.0",
+            "pandas>=2.2.3",
+            "requests>=2.32.3",
+            "tabulate>=0.9.0",
+            "nwshared @ git+https://github.com/numbworks/nwshared.git@v1.8.0#egg=nwshared&subdirectory=src"
+        ],
+        python_requires = ">=3.12",
+        license = "MIT"
+    )
