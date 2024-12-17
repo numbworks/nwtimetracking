308068f3acb07e7739432f398dea1628dff070c8
Rubèn <numbworks@gmail.com>
2024-12-17 17:16:02 +0000

    #42 setup.py, removed duplicate "pyarrow>=17.0.0".

--- src/setup.py
+++ src/setup.py
@@ -20,7 +20,6 @@ if __name__ == "__main__":
         install_requires = [
             "numpy>=2.1.2",
             "pyarrow>=17.0.0",
-            "pyarrow>=17.0.0",
             "pandas>=2.2.3",
             "requests>=2.32.3",
             "tabulate>=0.9.0",
